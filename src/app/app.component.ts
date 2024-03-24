2070b6d0e49ed04b86712e2e96a00140741a38e3
Cao Sy Hai Long <caosyhailong99@gmail.com>
2024-03-24 08:38:28 +0700

    Add checking password strength feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,6 +17,7 @@ export class AppComponent {
   prism: HTMLDivElement | undefined;
   title = 'loginapp';
   errorText = '';
+  isNotValidEmail = false;
   isNotMatched = false;
 
   loginForm = new FormGroup({
@@ -24,20 +25,27 @@ export class AppComponent {
     password: new FormControl<string>(''),
   });
 
-  registerForm = new FormGroup({
-    email: new FormControl<string>(''),
-    password: new FormControl<string>(''),
-    confirmPassword: new FormControl<string>(''),
+  registerForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', [Validators.required, this.validatePassword]],
+    confirmPassword: ['', Validators.required],
   });
 
-  constructor(private authService: AuthService) {}
+  get registeredEmail() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
 
   ngAfterViewInit() {
     this.prism = this.recPrism?.nativeElement;
   }
 
   onSubmit(event: any) {
-    console.log(event);
     event.preventDefault();
     let body = { ...this.loginForm.value };
     this.authService.login(body).subscribe((res: { result: string }) => {
@@ -72,21 +80,41 @@ export class AppComponent {
 
   onSubmitRegister() {
     let registerInfo = this.registerForm.value;
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(registerInfo.email as string))
-      console.log('Email OK');
-    else
-      console.log('Not a valid email');
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(registerInfo.email as string)) {
+      this.isNotValidEmail = false;
+    }
+    else {
+      this.isNotValidEmail = true;
+      return;
+    }
     if (registerInfo.password !== registerInfo.confirmPassword)
       this.isNotMatched = true;
     else {
       this.isNotMatched = false;
-      let mockInfo: any = registerInfo;
+      let mockInfo: any = {...registerInfo};
       delete mockInfo.confirmPassword;
       // mockInfo: {username: string, password: string}
-      console.log(mockInfo);
       this.authService
         .register(mockInfo as { username: string; password: string })
         .subscribe(() => {});
     }
+  }
+
+  validatePassword(control: FormControl) {
+    let isValid = true;
+    const validationRegex = [
+      { regex: /.{8,}/ }, // min 8 letters,
+      { regex: /[0-9]/ }, // numbers from 0 - 9
+      { regex: /[a-z]/ }, // letters from a - z (lowercase)
+      { regex: /[A-Z]/}, // letters from A-Z (uppercase),
+      { regex: /[^A-Za-z0-9]/} // special characters
+    ];
+      if(control.value) {
+        for(let test of validationRegex) {
+          isValid = test.regex.test(control.value);
+          if(!isValid) break;
+        }
+      } else isValid = false;
+      return !isValid ? { notValid: { value: control.value } } : null;
   }
 }
